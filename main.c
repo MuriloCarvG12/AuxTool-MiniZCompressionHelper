2c8a49820a066295880b8956bf1e4efdc64cd7d4
Murilo Carvalho <MuriloCarvalho2583@Outlook.com>
2026-06-08 15:52:42 -0300

    Feature: adds the extract file operation!

--- main.c
+++ main.c
@@ -26,8 +26,8 @@ int main(int argc, char *argv[])
     }
 
     char* Operation = argv[1];
-    char* FilePathArchive = argv[2];
-    char* FilePathCompression = argv[3];
+    char* FileToBeCompressed = argv[2];
+    char* PathToCompressedFiles = argv[3];
     int OperationSelected = 0;
     void *FileBuffer;
     HANDLE FileHandle;
@@ -55,21 +55,21 @@ int main(int argc, char *argv[])
         goto EXIT;
     }
 
-    if (FilePathArchive == "")
+    if (FileToBeCompressed == "")
     {
         printf("The path of the file was not informed!\n");
 
         goto EXIT;
     }
 
-    if (FilePathCompression == "")
+    if (PathToCompressedFiles == "")
     {
         printf("The path of the compressed file was not informed!\n");
 
         goto EXIT;
     }
 
-    if (GetFileAttributesA(FilePathArchive) == INVALID_FILE_ATTRIBUTES)
+    if (GetFileAttributesA(FileToBeCompressed) == INVALID_FILE_ATTRIBUTES)
     {
         printf("the file specified does not exist!\n");
         goto EXIT;
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
     if (OperationSelected == OPERATION_ADD)
     {
         FileHandle = CreateFileA(
-            FilePathArchive,
+            FileToBeCompressed,
             GENERIC_READ,
             FILE_SHARE_READ,
             NULL,
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
             goto EXIT;
         }
 
-        printf("[+] The file %s has been found and was opened for reading!\n", FilePathArchive);
+        printf("[+] The file %s has been found and was opened for reading!\n", FileToBeCompressed);
 
 
 
@@ -126,8 +126,8 @@ int main(int argc, char *argv[])
         if ((
             mz_zip_add_mem_to_archive_file_in_place
             (
-                FilePathCompression,
-                FilePathArchive,
+                PathToCompressedFiles,
+                FileToBeCompressed,
                 FileBuffer,
                 FileSize.QuadPart,
                 "",
@@ -137,11 +137,62 @@ int main(int argc, char *argv[])
         {
             DWORD Error;
             Error = ERROR_COMPRESSED_FILE_NOT_SUPPORTED;
-            printf("Failed to compress the file %s to archive %s! %d", FilePathArchive, FilePathCompression, Error);
+            printf("Failed to compress the file %s to archive %s! %d", FileToBeCompressed, PathToCompressedFiles, Error);
             return Error;
         }
 
-        printf("[+] The file %s has been successfully added to the archive!\n", FilePathArchive);
+        printf("[+] The file %s has been successfully added to the archive %s!\n", FileToBeCompressed, PathToCompressedFiles);
+
+    }
+
+    else if (OperationSelected == OPERATION_REMOVE)
+    {
+        mz_zip_archive ZipArchive = { 0 };
+        int NumberOfFilesInCompressedFile = 0;
+
+
+        if ((mz_zip_reader_init_file(&ZipArchive, PathToCompressedFiles, 0)) == FALSE)
+        {
+            printf("Mz_zip_reader_init_file failed with error code %d!\n", mz_zip_get_last_error(&ZipArchive));
+            goto EXIT;
+        }
+
+        printf("Opened the compressed file for reading!\n");
+
+        NumberOfFilesInCompressedFile = (int)mz_zip_reader_get_num_files(&ZipArchive);
+
+        printf("%d files were found inside the compressed file!\n", NumberOfFilesInCompressedFile);
+
+        for (int CurrentFile = 0; CurrentFile < NumberOfFilesInCompressedFile; CurrentFile++)
+        {
+            mz_zip_archive_file_stat CompressedFileStatistics = { 0 };
+
+            if (mz_zip_reader_file_stat(&ZipArchive, CurrentFile, &CompressedFileStatistics) == FALSE)
+            {
+
+                printf("Mz_zip_reader_file_stat failed with error code %d!\n", mz_zip_get_last_error(&ZipArchive));
+                goto EXIT;
+            }
+
+            if (_stricmp(CompressedFileStatistics.m_filename, FileToBeCompressed) == 0)
+            {
+                printf("[-] File %s found in archive %s.\n", FileToBeCompressed, PathToCompressedFiles);
+
+                if ((mz_zip_reader_extract_to_file(&ZipArchive, CurrentFile, FileToBeCompressed, 0)) == FALSE)
+                {
+                    printf("Mz_zip_reader_extract_to_file failed with error code %d!\n", mz_zip_get_last_error(&ZipArchive));
+                    goto EXIT;
+                }
+                else
+                {
+                    printf("[-] Successfully extracted file %s.\n", FileToBeCompressed);
+                }
+
+                goto EXIT;
+            }
+        }
+
+        printf("ERROR: File %s not found in archive %s!\n", FileToBeCompressed, PathToCompressedFiles);
 
     }
 
